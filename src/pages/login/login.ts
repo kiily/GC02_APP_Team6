6ce709788cd416812e95526021b87317416586c6
Miguel Marin <miguelmarin95@hotmail.com>
2017-01-02 00:18:44 +0100

    Authentication Features Added
    I added the following features:
    - Authentication is now working
    - Only Authenticated users can log in
    - Register user creates a hardcoded user (will fix this code once
    Brian's sign-up slides are ready)
    - log in button is disabled unless both email and password fields are
    filled
    - app returns an alert if the credentials are invalid
    - can press forgot password but I could not make it send a recovery
    email (tried to follow the firebase suggestion but it didn't work)
    
    Suggestions:
    - change the button for the new account to display "Register"

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { AngularFire, AuthMethods, AuthProviders, FirebaseListObservable} from 'angularfire2';
+import { FormBuilder, Validators, } from '@angular/forms';
+
+
+
 /*
   Generated class for the Login page.
 
@@ -13,16 +18,199 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+  loginForm;
+  //in case of invalid credentials.
+  error; 
+  
+  users: FirebaseListObservable<any []>;
+  recoveryEmail;
+  
+  
+  constructor(public navCtrl: NavController, public af : AngularFire, public formBuilder:FormBuilder,
+  public alertCtrl : AlertController) {
+
+//the validator ensures this doesn't work if no value is entered
+    this.loginForm = formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+
+      
+    });
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+
+    //creates users node in database
+    this.users = this.af.database.list('/users');
+    // //subscribe returns an obe=servable
+    // this.af.auth.subscribe(authState => {
+      
+      
+    //   //authState.uid
+      
+    // });
   }
 
-  signInBtnTouched(){
-    this.navCtrl.setRoot(HomePage);
+
+//pressing the sign-up button
+  login(){
+     
+    let email = this.loginForm.controls.email.value;
+    let password = this.loginForm.controls.password.value;
+    console.log(email);
+    console.log(password);
+
+
+    this.af.auth.login({
+      email: email,
+      password: password
+    },{
+      method: AuthMethods.Password,
+      provider: AuthProviders.Password
+         
+    })//redirect to page inside the promise
+    .then(authState =>  {
+    console.log("LOGIN-THEN", authState);
+    this.navCtrl.setRoot(HomePage)
+    })
+    .catch(error => {
+    console.log("LOGIN-ERROR", error);
+
+
+    //separate alert into new method
+    let alert = this.alertCtrl.create({
+
+      title: "Invalid credentials...",
+      subTitle: "The username and password you entered does not match our records.\nPlease try again.",
+      buttons: [
+        {
+          text: "OK",
+          //checking if it works
+          handler: data => {
+            console.log('OK clicked')
+          }
+        },
+        {
+          //did not implement recovery yet
+         text: "Forgot password?", 
+         handler: data => {
+           console.log("Forgot password clicked");
+           
+          this.passwordRecoveryPrompt();
+
+
+         }
+        }
+      ]
+    });
+    alert.present();
+  
+});
+
+
+  
+    
   }
+
+
+
+//for email : password auth
+  register() {
+    this.af.auth.createUser({
+      email: "miguelmarin95@hotmail.com",
+      //by default firebase makes passwords be at least 6 characters
+      //we should make an alert to show this
+      password: "password"
+
+      
+      
+      //resolve promise and catch errors in registration, if any
+    }).then(authState => {
+      //send email verification
+      //authState.auth.sendEmailVerification();
+      
+      let uid = authState.uid;
+      
+      console.log(uid);
+
+     
+     this.users.push({
+
+      userId: uid,
+      firstname: "Miguel",
+      lastname: "Marin",
+      email: "miguelmarin95@hotmail.com",
+      numberGP: 7889
+
+    });
+      
+      
+    })
+    .catch(error => {
+    
+    
+   console.log("REGISTER ERROR", error);
+    });
+
+
+  }
+
+//prompts the user for their email if they want to reset the password
+//this method could probably go into a Firebase provider
+passwordRecoveryPrompt() {
+
+
+      let prompt = this.alertCtrl.create({
+      title: 'Password recovery',
+      message: "Enter the email associated with your account:",
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+            this.recoveryEmail = data.email;
+            console.log('Confirm clicked');
+            console.log("prompt method: "+this.recoveryEmail);
+
+           
+           //did not manage to do this
+          //   var auth = firebase.auth();
+          //   auth.sendPasswordResetEmail(this.recoveryEmail).then(() => {
+          // console.log("recovery email sent");
+
+          // }).catch(() => {
+          //     console.log("an error happened")
+          // });
+            
+            
+          }
+        }
+      ]
+    });
+    prompt.present();
+    
+    
+  }
+
+
+  
 
 }
+
+
+
+
+
